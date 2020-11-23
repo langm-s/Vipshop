f0009c994949940974c32a5500b5ea1c8da5ebe3
langm-s <1661408651@qq.com>
2020-11-23 22:03:05 +0800

    首页布局完成，登录界面构建

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-document.querySelector('h1').onclick = function (){
-  alert(123);
-};
+var area = document.querySelector('#area');
+var areaPro = document.querySelector('.area');
+var closeArea = document.querySelector('.close')
+// 设置选择地区的显示与隐藏
+area.onclick = function(){
+  console.log(11)
+    areaPro.style.display = 'block';
+}
+closeArea.onclick = function(eve){
+  var e = eve || event;
+  e.stopPropagation();//阻止事件冒泡
+  areaPro.style.display = 'none';
+}
+area.onblur = function(){
+  areaPro.style.display = 'none';
+}
